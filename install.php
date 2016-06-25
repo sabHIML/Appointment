f492d8d191ed1df35a69ac837f8f3ac55d2c34c5
Ahtasham Himel <himel.46@gmail.com>
2016-06-25 13:37:59 +0600

    Improved project installing process

--- install.php
+++ install.php
@@ -5,6 +5,20 @@ if(php_sapi_name() !== 'cli') {
     exit;
 }
 
+//initiating composer autoloader
+$command = "composer dump-autoload -o";
+$output = array();
+exec($command,$output,$worked);
+switch($worked){
+    case 0:
+        echo "Project successfully installed \n";
+        break;
+    case 1:
+        echo "There was an error during work with composer \n";
+        echo "Aborted \n";
+        break;
+}
+
 require_once 'vendor/autoload.php';
 require_once 'src/config/install.php';
 
--- src/config/install.php
+++ src/config/install.php
@@ -11,28 +11,10 @@ class Install {
             if(CalendarMapper::init())
                 echo "Database table created successfully \n";
 
-            $this->composer_init();
-
         } catch(\Exception $e) {
             echo 'ERROR : '. $e->getMessage();
             exit;
         }
     }
 
-    private function composer_init() {
-
-        //initiating composer autoloader
-        $command = "composer dump-autoload -o";
-        $output = array();
-        exec($command,$output,$worked);
-        switch($worked){
-            case 0:
-                echo "Project successfully installed \n";
-                break;
-            case 1:
-                echo "There was an error during work with composer \n";
-                echo "Aborted \n";
-                break;
-        }
-    }
 }
